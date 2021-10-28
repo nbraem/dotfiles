23233b0cf84f3abb38bfd9a8c2157c8ccba72d16
Nicolas Braem <nicolas.braem@gmail.com>
2021-10-28 10:31:44 +0200

    use pyhome instead of homesick

--- dotinstall.sh
+++ dotinstall.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-sudo apt-get install -y ruby
-sudo gem install homesick 
-homesick clone nbraem/dotfiles && homesick symlink dotfiles --force
-homesick clone nbraem/dotvim && homesick symlink dotvim
+pip install --user pyhome
+pyhome clone https://github.com/nbraem/dotfiles.git && pyhome link dotfiles
+pyhome clone https://github.com/nbraem/dotvim.git && pyhome link dotfiles
 source ~/.bashrc
-sudo apt-get install -y exuberant-ctags silversearcher-ag software-properties-common
-
-# vim 8
-sudo add-apt-repository -y ppa:jonathonf/vim
-sudo apt-get update
-sudo apt-get install -y vim
+sudo apt-get install -y exuberant-ctags silversearcher-ag software-properties-common vim
