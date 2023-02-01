8b3bcaf134a012a39f809f4617e0017809f2860b
Nicolas Braem <nicolas.braem@gmail.com>
2023-02-01 10:40:21 +0100

    Update dotinstall.sh

--- dotinstall.sh
+++ dotinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pip install --user pyhome
+python3 -m pip install --user pyhome
 pyhome clone https://github.com/nbraem/dotfiles.git && pyhome link dotfiles
-pyhome clone https://github.com/nbraem/dotvim.git && pyhome link dotfiles
+pyhome clone https://github.com/nbraem/dotvim.git && pyhome link dotvim
 source ~/.bashrc
 sudo apt-get install -y exuberant-ctags silversearcher-ag software-properties-common vim
